1df2ff0f270d07ea0331e764c753627ed8c226df
kiakd <pame7981@gmail.com>
2024-05-08 18:35:24 +0700

    update code

--- resources/views/Backend/campaign/create.blade.php
+++ resources/views/Backend/campaign/create.blade.php
@@ -123,17 +123,20 @@
 @endsection
 
 @section('scripts')
-    <script src="//cdn.ckeditor.com/4.22.1/full/ckeditor.js"></script>
+    {{-- <script src="//cdn.ckeditor.com/4.22.1/full/ckeditor.js"></script> --}}
+    <script src="{{ URL::asset('backend/assets/libs/ckeditor/ckeditor.js') }}"></script>
     <script>
 CKEDITOR.config.allowedContent = true;
         CKEDITOR.replace('editor', {
             filebrowserUploadUrl: "{{ route('upload', ['_token' => csrf_token()]) }}",
             filebrowserUploadMethod: 'form',
+            extraPlugins: 'youtube',
         });
 
         CKEDITOR.replace('editor2', {
             filebrowserUploadUrl: "{{ route('upload', ['_token' => csrf_token()]) }}",
             filebrowserUploadMethod: 'form',
+            extraPlugins: 'youtube',
         });
         function readURL01(input) {
             if (input.files && input.files[0]) {
